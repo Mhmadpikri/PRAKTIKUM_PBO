dfbf4194fc6d4c8f453180b54964c358659e9387
Mhmadpikri <92666266+Mhmadpikri@users.noreply.github.com>
2023-04-04 15:04:46 +0800

    Add files via upload

---
+++ postest5/src/postest5/ZakatFitrah.java
@@ -0,0 +1,45 @@
+
+package postest5;
+
+public final class ZakatFitrah extends Zakat {
+    private int jumlah_zakat;
+    private String sumber_dana;
+    public final String jenis = "Zakat Fitrah"; 
+    
+    public ZakatFitrah(String nama, String alamat, int sedekah, int jumlah_zakat1, String sumber_dana1, String jenis){
+         super(nama, alamat, sedekah);
+        
+        this.jumlah_zakat = jumlah_zakat1;
+        this.sumber_dana = sumber_dana1;
+    }
+
+    public int getJumlah_zakat1() {
+        return jumlah_zakat;
+    }
+
+    public void setJumlah_zakat1(int jumlah_zakat1) {
+        this.jumlah_zakat = jumlah_zakat1;
+    }
+
+    public String getSumber_dana1() {
+        return sumber_dana;
+    }
+
+    public void setSumber_dana1(String sumber_dana1) {
+        this.sumber_dana = sumber_dana1;
+    }
+    
+    public void tampilzakat(){
+                   System.out.println("|= Nama Muzzaki       : " + this.nama);
+                   System.out.println("|= Alamat Muzzaki     : " + this.alamat);
+                   System.out.println("|= Jumlah Zakat       : " + this.jumlah_zakat);
+                   System.out.println("|= Jumlah Sedekah     : " + this.sedekah);
+                   System.out.println("|= Jumlah Zakat       : " + this.sumber_dana);
+                   System.out.println("|==================================================|");
+                   System.out.println("\n");
+    }
+    
+    public void tampilzakat(String judul){
+        System.out.println(judul);
+    }
+}
